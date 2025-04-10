522b30bc2b5a6b04df5af5df220f69286372185c
cristiancrakl <moralescadenac@gmail.com>
2025-04-09 20:35:20 -0500

    subir parte nueva

--- Js/javascript.js
+++ Js/javascript.js
@@ -4,25 +4,25 @@ document.getElementById("formLogin").addEventListener("submit", function (e) {
     e.preventDefault();
     const email = document.getElementById("Email").value;
     const password = document.getElementById("Password").value;
-    
-    let alertType=""
-    let message=""
+
+    let alertType = ""
+    let message = ""
 
     if (email === "" || password === "") {
-        alertType="warning"
-        message="inicio de sesion extitoso"
+        alertType = "warning"
+        message = "inicio de sesion extitoso"
 
     } else if (email === 'moralescadenac@gmail.com' && password === '123456') {
 
-        alertType="success"
-        message="inicio de sesion extitoso"
+        alertType = "success"
+        message = "inicio de sesion extitoso"
 
     } else {
-        alertType="danger"
-        message="contraseña o correo incorrectas"
+        alertType = "danger"
+        message = "contraseña o correo incorrectas"
     }
 
-    let alert= `
+    let alert = `
     
     <div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
   ${message}
@@ -30,6 +30,20 @@ document.getElementById("formLogin").addEventListener("submit", function (e) {
 </div>
 
     `;
-    document.getElementById("alert").innerHTML=alert;
+    document.getElementById("alert").innerHTML = alert;
 
 })
+
+
+function login(email, password) {
+    fetch("https://reqres.in/api/login", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({email,password})
+    })
+    .then(
+        
+    )
+}
