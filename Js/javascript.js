9758a6be22052a79abf716f9d77ae32690df2ccf
cristiancrakl <moralescadenac@gmail.com>
2025-04-09 20:13:33 -0500

    subiendo primeros cambios

---
+++ Js/javascript.js
@@ -0,0 +1,35 @@
+document.getElementById("formLogin").addEventListener("submit", function (e) {
+
+
+    e.preventDefault();
+    const email = document.getElementById("Email").value;
+    const password = document.getElementById("Password").value;
+    
+    let alertType=""
+    let message=""
+
+    if (email === "" || password === "") {
+        alertType="warning"
+        message="inicio de sesion extitoso"
+
+    } else if (email === 'moralescadenac@gmail.com' && password === '123456') {
+
+        alertType="success"
+        message="inicio de sesion extitoso"
+
+    } else {
+        alertType="danger"
+        message="contraseña o correo incorrectas"
+    }
+
+    let alert= `
+    
+    <div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
+  ${message}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+
+    `;
+    document.getElementById("alert").innerHTML=alert;
+
+})
